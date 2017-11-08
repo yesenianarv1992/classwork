9eda50964607210ef84425f946543b6effff3d23
Yesenia Narvaez <yesenian1992@gmail.com>
2017-11-08 00:51:57 +0000

    first commit

---
+++ reverseAstring.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+  return str;
+}
+
+reverseString("hello");
+
+function reverseString(str) {
+var array = [];
+array = str.split("");
+arr = array.reverse();
+return arr.join("");
+}
+console.log(reverseString("Yesenia Narvaez"))
